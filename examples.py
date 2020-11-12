25ebf2aa6b7bca32def45fbee488511739919a27
mBerdal <magberd@stud.ntnu.no>
2020-11-12 13:22:36 +0100

    started adding objective function derivation

--- examples.py
+++ examples.py
@@ -43,3 +43,36 @@ def plot_vis_set_example():
   ax.axis("off")
   plt.savefig('../oppgave_tekst/figs/vis_set_example1.pdf', format='pdf')
   plt.show()
+
+def plot_trilateration():
+  S = np.array([
+    [0, 1, 2],
+    [0, 1, 0]
+  ])
+
+  p = np.array([0.3, 0.6])
+  _, ax = plt.subplots()
+  ax.scatter(*S, color="b")
+  D = np.linalg.norm(p.reshape(2, 1) - S, axis=0)
+  theta = np.linspace(0, 2*np.pi)
+  for i in range(3):
+    ax.plot(S[0, i] + D[i]*np.cos(theta), S[1, i] + D[i]*np.sin(theta), "k--")
+    ax.plot(
+      [S[0, i], S[0, i] + D[i]*np.cos(0)],
+      [S[1, i], S[1, i] + D[i]*np.sin(0)],
+      "b"
+    )
+    str_d = "$d_{" + str(i) + "}$"
+    ax.annotate(str_d, xy = (S[0, i] + D[i]/2, S[1, i]+0.1))
+    str_s = "$\mathbf{S}_{" + str(i) + "}$"
+    ax.annotate(str_s, xy = (S[0, i], S[1, i]+0.05))
+  ax.scatter(*p, color="#4eb53e", zorder=100)
+  ax.annotate("$\mathbf{p}$", xy=(p[0]+0.1, p[1]))
+  ax.set_facecolor("#ededed")
+  ax.axis("equal")
+  ax.axis("off")
+  plt.savefig('report/figs/trilateration_example.pdf', format='pdf')
+  plt.show()
+
+if __name__ == "__main__":
+  plot_trilateration()
